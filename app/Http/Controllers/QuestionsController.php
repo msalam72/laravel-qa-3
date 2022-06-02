dddc946c4fc0672cd32e507a16140d95221a2e07
Maher Salam <msalam72@gmail.com>
2022-06-02 17:05:02 +0400

    Implement form validation and store question

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Question;
 use Illuminate\Http\Request;
+use App\Http\Requests\AskQuestionRequest;
 
 class QuestionsController extends Controller
 {
@@ -13,10 +14,10 @@ class QuestionsController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {        
+    {
         $questions = Question::with('user')->latest()->paginate(10);
 
-        return view('questions.index', compact('questions'));        
+        return view('questions.index', compact('questions'));
     }
 
     /**
@@ -37,9 +38,10 @@ class QuestionsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(AskQuestionRequest $request)
     {
-        //
+        $request->user()->questions()->create($request->only('title', 'body'));
+        return redirect()->route('questions.index')->with('success', "Your question has been submitted");
     }
 
     /**
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -12,35 +12,36 @@
                             <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">Ask Question</a>
                         </div>
                     </div>
-                    
+
                 </div>
 
                 <div class="card-body">
-                   @foreach ($questions as $question)
+                    @include ('layouts._messages')
+                    @foreach ($questions as $question)
                         <div class="media">
                             <div class="d-flex flex-column counters">
                                 <div class="vote">
                                     <strong>{{ $question->votes }}</strong> {{ str_plural('vote', $question->votes) }}
-                                </div>                            
+                                </div>
                                 <div class="status {{ $question->status }}">
                                     <strong>{{ $question->answers }}</strong> {{ str_plural('answer', $question->answers) }}
-                                </div>                            
+                                </div>
                                 <div class="view">
                                     {{ $question->views . " " . str_plural('view', $question->views) }}
-                                </div>                            
+                                </div>
                             </div>
                             <div class="media-body">
                                 <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                                 <p class="lead">
-                                    Asked by 
-                                    <a href="{{ $question->user->url }}">{{ $question->user->name }}</a> 
+                                    Asked by
+                                    <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
                                     <small class="text-muted">{{ $question->created_date }}</small>
                                 </p>
                                 {{ str_limit($question->body, 250) }}
-                            </div>                        
+                            </div>
                         </div>
                         <hr>
-                   @endforeach
+                    @endforeach
 
                     <div class="mx-auto">
                         {{ $questions->links() }}
